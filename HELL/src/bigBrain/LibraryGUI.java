b730fb5567d43f58004eb518fdad3800ba832346
20ncherry <20ncherry@WA-248-9580.schools.westford-ma.gov>
2019-11-18 13:38:01 -0500

    Big Brain

---
+++ HELL/src/bigBrain/LibraryGUI.java
@@ -0,0 +1,14 @@
+package bigBrain;
+
+import javax.swing.*;
+import BreezySwing.*;
+
+public class LibraryGUI {
+
+	public static void main(String[] args) {
+		JButton enter = addButton("Enter a new book",1,1,1,1);
+		JButton checkOut = addButton("Check Out a Book")
+
+	}
+
+}
