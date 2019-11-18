575d659814a1836ce771bb799cf81571e4fed70b
20ncherry <20ncherry@WA-248-9580.schools.westford-ma.gov>
2019-11-18 13:55:40 -0500

    balls

--- HELL/src/bigBrain/LibraryGUI.java
+++ HELL/src/bigBrain/LibraryGUI.java
@@ -3,12 +3,41 @@ package bigBrain;
 import javax.swing.*;
 import BreezySwing.*;
 
-public class LibraryGUI {
+public class LibraryGUI extends GBFrame {
+
+	JButton enter = addButton("Enter a new book", 1, 1, 1, 1);
+	JButton checkOut = addButton("Check Out a Book", 1, 2, 1, 1);
+	JButton returnBook = addButton("return a book", 1, 3, 1, 1);
+	JButton outputButton = addButton("Check Availability", 1, 4, 1, 1);
+	JButton overdueButton = addButton("Check OverdueBooks", 1, 5, 1, 1);
+	JButton searchButton = addButton("Search for a Book", 1, 6, 1, 1);
 
 	public static void main(String[] args) {
-		JButton enter = addButton("Enter a new book",1,1,1,1);
-		JButton checkOut = addButton("Check Out a Book")
+		JFrame frm = new LibraryGUI();
+		frm.setTitle("Ace Program");
+		frm.setSize(700, 500);
+		frm.setVisible(true);
+	}
 
+	public void buttonClicked(JButton buttonObj) {
+		if (buttonObj == enter) {
+
+		}
+		if (buttonObj == checkOut) {
+
+		}
+		if (buttonObj == returnBook) {
+
+		}
+		if (buttonObj == outputButton) {
+
+		}
+		if (buttonObj == overdueButton) {
+
+		}
+		if (buttonObj == searchButton) {
+
+		}
 	}
 
 }
